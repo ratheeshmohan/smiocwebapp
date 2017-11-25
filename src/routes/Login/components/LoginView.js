2d87faad3ce36a5e92037876908d572fea7bd577
RM <ratheeshmohan@gmail.com>
2017-11-26 07:51:52 +1100

    login page

--- src/routes/Login/components/LoginView.js
+++ src/routes/Login/components/LoginView.js
@@ -1,36 +1,45 @@
 import React from "react";
 import "./LoginView.scss";
+import { Button, Form, Input, Label, FormGroup } from "reactstrap";
 
 export const LoginView = () => (
   <div className="wrapper">
-    <form className="form-signin">
-      <h2 className="form-signin-heading">Sign in</h2>
-      <label for="inputEmail" className="sr-only">
-        Email address
-      </label>
-      <input
-        type="email"
-        id="inputEmail"
-        className="form-control"
-        placeholder="Email address"
-        required
-        autofocus
-      />
-      <label for="inputPassword" className="sr-only">
-        Password
-      </label>
-      <input
-        type="password"
-        id="inputPassword"
-        className="form-control"
-        placeholder="Password"
-        required
-      />
-      <button className="btn btn-lg btn-primary btn-block" type="submit">
+    <Form className="form-signin">
+      <h2>Sign in</h2>
+
+      <FormGroup check>
+        <Label for="inputEmail" className="sr-only">
+          Email address
+        </Label>
+        <Input
+          type="email"
+          name="email"
+          id="inputEmail"
+          placeholder="Email address"
+          required
+          autofocus
+        />
+      </FormGroup>
+
+      <FormGroup check>
+        <Label for="inputPassword" className="sr-only">
+          Password
+        </Label>
+        <Input
+          type="password"
+          name="email"
+          id="inputPassword"
+          placeholder="Password"
+          required
+          autofocus
+        />
+      </FormGroup>
+
+      <Button color="primary" size="lg" block>
         Login
-      </button>
+      </Button>
       <a href="/resetpassword">Forgot password</a>
-    </form>
+    </Form>
   </div>
 );
 
