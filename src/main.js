74f465b784a4edd71c964ee2031947642da8f3d8
RM <ratheeshmohan@gmail.com>
2017-12-01 19:57:50 +1100

    removed bootstrap & added MDC

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import createStore from './store/createStore'
 import './styles/main.scss'
+import '../node_modules/material-components-web/dist/material-components-web.css'
 
 // Store Initialization
 // ------------------------------------
